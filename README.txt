0443af49ec16db17267bed765686ca2e836224f1
sakares <sakares@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-24 04:30:01 +0000

    release plugin version 1.2
    git-svn-id: https://plugins.svn.wordpress.org/adsoptimal/tags/1.2@995858 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.txt
@@ -0,0 +1,70 @@
+=== Mobile Ad for Wordpress by AdsOptimal ===
+Contributors: jasukkas, baddth, jojas
+Tags: ads, mobile ad, mobile ads, monetization, make money, iphone, android, mobile web
+Requires at least: 3.5.1
+Tested up to: 3.8
+Stable tag: 1.2
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+Mobile Ad for Wordpress by AdsOptimal
+
+Earn top dollar for your mobile web traffic! Promote mobile offers on your Wordpress site and get up to $15 RPM or $4 per download by just installing the plugin.
+
+Features
+
+* Choose from 9 different ad experiences, such as banner, interstitial, pop-up, and more.
+* Control how frequent user can see the ad. You can make sure you don't bug user too many times. 
+* Select to show the ad only when user finish reading your content.
+* Take only minutes to install. See your revenue in real time.
+* Receive a payout via Paypal or Check.
+
+== Installation ==
+
+1. Download this repository in zip file and add it to your Wordpress plugin
+2. Activate plugin
+3. Log in or sign up with your AdsOptimal account
+4. (optional) Customize your ad experience from plugin setting
+
+Now your Wordpress site is ready to promote the ad and make money for you.
+
+== Additional Info ==
+
+About AdsOptimal ([company profile](http://www.adsoptimal.com/company))
+
+Visit us at [AdsOptimal website](http://www.adsoptimal.com)
+Contact us at [team@adsoptimal.com](mailto:adsoptimal.com)
+
+== Frequently Asked Questions ==
+
+= How is AdsOptimal different from other ad solutions? =
+Traditional ad solutions are designed for desktop web. In contrast, AdsOptimal is designed for mobile from the ground up. AdsOptimal delivers better mobile experience. For example, we optimize ad display to fit screen size and device orientation. We leverage touch experience and are specialized in running mobile campaigns. That makes you earn more money on your mobile traffic.
+
+= What will happen if my Wordpress template is currently not mobile optimized? =
+AdsOptimal displays perfect mobile ad no matter you have a mobile template or not. You can be sure that the your mobile revenue is maximized regardless to your template.
+
+= Will AdsOptimal impact my Wordpress site on desktop? =
+No, AdsOptimal leave your desktop site untouched. So, you can keep existing desktop revenue and get extra revenues from mobile.
+
+= When will I get paid? =
+We offers payout via Paypal and Check. Please let us know if you have restriction in receving Payout. You can always request a payout anytime when your balance reach the minimum requirement.
+
+= What is the cost? =
+There is no cost to sign up. In fact, we offer a promotion first time user to try out. If you do not like the solution, you can turn off the account anytime. No string attached.
+
+= More questions? =
+Send us email at [team@adsoptimal.com](mailto:adsoptimal.com). We are happy to answer your questions.
+
+== Changelog ==
+No change log at this moment.
+
+== Upgrade Notice ==
+No upgrade notice at this moment.
+
+== Screenshots ==
+1. Choose ad experience that fit with your Wordpress site.
+2. Preview of our mobile ad. App-Store-like experience.
+3. Preview of our mobile ad. Standard banner format.
+4. Preview of our mobile ad. Contextual style. 
---
+++ admin/views/admin.php
@@ -0,0 +1,214 @@
+<?php
+/**
+ * Represents the view for the administration dashboard.
+ *
+ * This includes the header, options, and other information that should provide
+ * The User Interface to the end user.
+ *
+ * @package   AdsOptimal
+ * @author    team@adsoptimal.com
+ * @license   GPL-2.0+
+ * @link      http://www.adsoptimal.com
+ * @copyright 2014 Your Name or Company Name
+ */
+	if (isset($_POST["adsoptimal_access_token"])) update_option('adsoptimal_access_token', $_POST["adsoptimal_access_token"]);
+	if (isset($_POST["adsoptimal_email"])) update_option('adsoptimal_email', $_POST["adsoptimal_email"]);
+	if (isset($_POST["adsoptimal_publisher_id"])) update_option('adsoptimal_publisher_id', $_POST["adsoptimal_publisher_id"]);
+	if (isset($_POST["adsoptimal_settings"])) update_option('adsoptimal_settings', $_POST["adsoptimal_settings"]);
+?>
+<form method="post" id="myForm">
+<input type="hidden" name="adsoptimal_access_token" value="<?php echo get_option('adsoptimal_access_token', '') ?>">
+<input type="hidden" name="adsoptimal_email" value="<?php echo get_option('adsoptimal_email', '') ?>">
+<input type="hidden" name="adsoptimal_publisher_id" value="<?php echo get_option('adsoptimal_publisher_id', '') ?>">
+<input type="hidden" name="adsoptimal_settings" value="<?php echo get_option('adsoptimal_settings', '{}') ?>">
+</form>
+<div class="authenticate" style="display: none;">
+	<div style="color: #ffffff; width: 350px; margin: 100px auto 0px; padding: 15px; border: solid 1px #ef4036; text-align: center; background-color: #f05a28; -webkit-border-radius: 7px; -moz-border-radius: 7px; border-radius: 7px;">
+		<h3 style="margin-top: 0px; font-weight: 300;">Connect Your AdsOptimal Account</h3>
+		<p style="padding: 50px 0;"><a class="btn btn-default connect" href="javascript:void(0);" style="font-size: 15px; line-height: 40px; padding: 0 30px;">Connect AdsOptimal</a></p>
+		<p>Promote mobile apps on your website and get $3 per download, The highest payout you'll find on the web.</p>
+	</div>
+</div>
+<div class="authenticating" style="display: none;">
+	<div style="color: #ffffff; width: 350px; margin: 100px auto 0px; padding: 15px; border: solid 1px #ef4036; text-align: center; background-color: #f05a28; -webkit-border-radius: 7px; -moz-border-radius: 7px; border-radius: 7px;">
+		<h3 style="margin-top: 0px; font-weight: 300;">Connect Your AdsOptimal Account</h3>
+		<h4 style="padding: 50px 0; line-height: 42px;">Retrieving Your Account..</h4>
+		<p>Promote mobile apps on your website and get $3 per download, The highest payout you'll find on the web.</p>
+	</div>
+</div>
+<div class="authenticated" style="display: none;">
+	<div style="background-color: #f05a28; min-width: 1050px;">
+		<img src="<?php echo plugins_url( '../assets/white-logo.png', __FILE__ ) ?>" style="width: 170px; height: 50px; float: left;">
+		<div class="btn-group pull-right" style="margin: 8px 10px 0 0;">
+			<button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown" style="background: transparent; border-color: #ffffff; color: #ffffff; ">
+				<span class="email-address" style="text-shadow: none;"></span> <span class="caret"></span>
+			</button>
+			<ul class="dropdown-menu">
+				<li><a class="change-account" href="javascript:void(0);">Change Account</a></li>
+			</ul>
+		</div>
+		<div class="btn-group pull-right" style="margin: 8px 10px 0 0;">
+			<button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown" style="background: transparent; border-color: #ffffff; color: #ffffff; ">
+				<span class="earning" style="text-shadow: none;">$0.00</span> <span class="caret"></span>
+			</button>
+			<ul class="dropdown-menu">
+				<li><a href="http://www.adsoptimal.com/customer/insights" target="_blank">Insights</a></li>
+				<li><a href="http://www.adsoptimal.com/customer/payout" target="_blank">Payout</a></li>
+			</ul>
+		</div>
+		<div style="clear: both;"></div>
+	</div>
+	<div class="adsoptimal-injection-container" style="width: 1050px; margin: 0 auto; background-color: #ffffff;">
+	</div>
+</div>
+<script type="text/javascript" charset="utf-8">
+var $ = jQuery;
+var settings = {
+	'host':     "www.adsoptimal.com"
+, 'clientId': "8d1ccad0433322bed59691fb0d6367a1f4846da1b70ce114cacc7202478e6cd9"
+};
+var VIEW = { AUTHENTICATE:0, AUTHENTICATING:1, AUTHENTICATED:2 };
+
+var AdsOptimal = {
+	openOAuth: (function() {
+		var popupWindow=null;
+		return function(redirect_uri, logout) {
+			url = settings.host + '/oauth/authorize?client_id=' + settings.clientId + '&redirect_uri=' + encodeURIComponent(redirect_uri) + '&response_type=token';
+			if (logout) url = settings.host + '/oauth/logout?redirect=' + encodeURIComponent(url);
+			
+			if(popupWindow && !popupWindow.closed) popupWindow.focus();
+			else popupWindow = window.open(url,"_blank","directories=no, status=no, menubar=no, scrollbars=yes, resizable=no,width=515, height=330,top=" + (screen.height - 330)/2 + ",left=" + (screen.width - 515)/2);
+			
+			function parent_disable() {
+				if(popupWindow && !popupWindow.closed) popupWindow.focus();
+			}
+			
+			$(document.body).bind('focus', parent_disable);
+			$(document.body).bind('click', parent_disable);
+		}
+	})(),
+	
+	extractQuery: function(hash, regex) {
+		var match = hash.match(regex);
+		return !!match && match[1];
+	},
+	switchView: function(view) {
+		$('div.authenticate').hide();
+		$('div.authenticating').hide();
+		$('div.authenticated').hide();
+		
+		switch(view) {
+			case VIEW.AUTHENTICATE:
+				$('div.authenticate').show();
+				break;
+			case VIEW.AUTHENTICATING:
+				$('div.authenticating').show();
+				break;
+			case VIEW.AUTHENTICATED:
+				$('div.authenticated').show();
+				break;
+		}
+	},
+	
+	updateSettings: function() {
+		if (window.disabledUpdateSettings) return;
+		AdsOptimal.saveSettings(false);
+	},
+	saveSettings: function(save) {
+		console.log('NOT IMPLEMENTED');
+	},
+	restoreSettings: function() {
+		console.log('NOT IMPLEMENTED');
+	},
+	
+	oAuthCallback: function(token) {
+		AdsOptimal.switchView(VIEW.AUTHENTICATING);
+		
+		$.ajax({
+				url: settings.host + '/api/v1/publisher_info'
+			, beforeSend: function (xhr) {
+					xhr.setRequestHeader('Authorization', "Bearer " + token);
+					xhr.setRequestHeader('Accept',        "application/json");
+				}
+			, success: function (response) {
+					if (response.data) {
+						AdsOptimal.switchView(VIEW.AUTHENTICATING);
+						$('[name="adsoptimal_access_token"]').val(token);
+						$('[name="adsoptimal_email"]').val(response.data.email);
+						$('[name="adsoptimal_publisher_id"]').val(response.data.publisher_id);
+						AdsOptimal.updateSettings();
+						$('#myForm')[0].submit();
+					} else {
+						AdsOptimal.switchView(VIEW.AUTHENTICATE);
+					}
+				}
+		});
+	},
+	initialize: function() {
+		$("a.connect").click(function() {
+			AdsOptimal.openOAuth(location.href.replace('#' + location.hash,""), false);
+		});
+		$(".change-account").click(function() {
+			AdsOptimal.openOAuth(location.href.replace('#' + location.hash,""), true);
+		});
+		
+		var token = AdsOptimal.extractQuery(window.location.hash, /access_token=(\w+)/);
+		var error = AdsOptimal.extractQuery(window.location.hash, /error=(\w+)/);
+		if (error == 'access_denied') {
+			window.close();
+		}
+		else if(token) {
+			try { window.opener.AdsOptimal.oAuthCallback(token); }
+			catch(ex) { }
+			window.close();
+		}
+		else {
+			AdsOptimal.switchView(VIEW.AUTHENTICATE);
+			
+			$('.email-address').text($('[name="adsoptimal_email"]').val());
+			
+			if ($('[name="adsoptimal_access_token"]').val()) {
+				AdsOptimal.switchView(VIEW.AUTHENTICATED);
+				
+				$('.earning').text('...');
+				if ($('[name="adsoptimal_access_token"]').val()) {
+					$.ajax({
+							url: settings.host + '/api/v1/insight_info'
+						, beforeSend: function (xhr) {
+								xhr.setRequestHeader('Authorization', "Bearer " + $('[name="adsoptimal_access_token"]').val());
+								xhr.setRequestHeader('Accept',        "application/json");
+							}
+						, success: function (response) {
+								if (response.data) {
+									$('.earning').text('$' + response.data.pending_payout.toFixed(2));
+								} else {
+									$('.earning').text('Insight');
+								}
+							}
+					});
+				}
+				
+				$.ajax({
+						url: settings.host + '/api/v1/settings_injection.html'
+					, beforeSend: function (xhr) {
+							xhr.setRequestHeader('Authorization', "Bearer " + $('[name="adsoptimal_access_token"]').val());
+							xhr.setRequestHeader('Accept',        "text/html");
+						}
+					, success: function (response) {
+							var container = $('.adsoptimal-injection-container');
+							response = response.replace('{{SITE}}', settings.host);
+							container.html(response);
+							
+							container.find('[src]').each(function() {
+								if ($(this).attr('src').indexOf('http://') == -1)
+									$(this).attr('src', settings.host + $(this).attr('src'));
+							});
+							AdsOptimal.restoreSettings();
+						}
+				});
+			}
+		}
+	}
+};
+AdsOptimal.initialize();
+</script>
