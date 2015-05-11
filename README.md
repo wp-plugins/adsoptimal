c601c1bb357586c72137e2a9c0aa80faaf9f5089
baddth <baddth@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-11 05:25:29 +0000

    Adding new format to the plugin.
    git-svn-id: https://plugins.svn.wordpress.org/adsoptimal/tags/1.4.0@1157673 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.md
@@ -0,0 +1,64 @@
+## Mobile Ad for Wordpress by AdsOptimal ##
+**Contributors:** jasukkas, baddth, sakares
+**Tags:** ads, mobile ad, mobile ads, monetization, make money, iphone, android, mobile web
+**Requires at least:** 3.5.1
+**Tested up to:** 3.8
+**Stable tag**: 1.4.0
+**License:** GPLv2 or later
+**License URI:** http://www.gnu.org/licenses/gpl-2.0.html
+
+## Description ##
+
+**Mobile Ad for Wordpress by AdsOptimal**
+
+AdsOptimal offers innovative ad unit, designed and optimized for mobile sites. We make the ad load faster and make it easier on various smart devices. The highest payout you'll find on the web. You don't need a mobile app or an optimized mobile experience to leverage mobile. Start by serving relevant ads to your mobile users with our solution. There's no risk to try!
+
+**Features**
+
+- Choose from 9 different ad experiences, such as banner, interstitial, pop-up, and more.
+- Control how frequent user can see the ad. You can make sure you don't bug user too many times. 
+- Select to show the ad after user finishes reading your content.
+- Take only minutes to install. See your revenue in real time.
+- Receive a payout via Paypal or Check.
+
+## Installation ##
+
+1. Download this repository in zip file and add it to your Wordpress plugin
+2. Activate plugin
+3. Log in or sign up with your AdsOptimal account
+4. (optional) Customize your ad experience from plugin setting
+
+Now your Wordpress site is ready to promote the ad and make money for you.
+
+## Additional Info ##
+
+About AdsOptimal ([company profile](http://www.adsoptimal.com/company))
+
+Visit us at [AdsOptimal website](http://www.adsoptimal.com)
+Contact us at [team@adsoptimal.com](mailto:adsoptimal.com)
+
+## Frequently Asked Questions ##
+
+**How is AdsOptimal different from other ad solutions?**
+Traditional ad solutions are designed for desktop web. In contrast, AdsOptimal is designed for mobile from the ground up. AdsOptimal delivers better mobile experience. For example, we optimize ad display to fit screen size and device orientation. We leverage touch experience and are specialized in running mobile campaigns. That makes you earn more money on your mobile traffic.
+
+**What will happen if my Wordpress template is currently not mobile optimized?**
+AdsOptimal displays perfect mobile ad no matter you have a mobile template or not. You can be sure that the your mobile revenue is maximized regardless to your template.
+
+**Will AdsOptimal impact my Wordpress site on desktop?**
+No, AdsOptimal leave your desktop site untouched. So, you can keep existing desktop revenue and get extra revenues from mobile.
+
+**When will I get paid?**
+We offers payout via Paypal and Check. Please let us know if you have restriction in receving Payout. You can always request a payout anytime when your balance reach the minimum requirement.
+
+**What is the cost?**
+There is no cost to sign up. In fact, we offer a promotion first time user to try out. If you do not like the solution, you can turn off the account anytime. No string attached.
+
+**More questions?**
+Send us email at [team@adsoptimal.com](mailto:adsoptimal.com). We are happy to answer your questions.
+
+## Changelog ##
+No change log at this moment.
+
+## Upgrade Notice ##
+No upgrade notice at this moment.
---
+++ adsoptimal.php
@@ -0,0 +1,207 @@
+<?php
+/**
+ * The WordPress Plugin Boilerplate.
+ *
+ * A foundation off of which to build well-documented WordPress plugins that
+ * also follow WordPress Coding Standards and PHP best practices.
+ *
+ * @package   AdsOptimal
+ * @author    team@adsoptimal.com
+ * @license   GPL-2.0+
+ * @link      http://www.adsoptimal.com
+ * @copyright 2014 AdsOptimal (Under Social Nation Inc)
+ *
+ * @wordpress-plugin
+ * Plugin Name:       AdsOptimal - Mobile Ad
+ * Plugin URI:        http://www.adsoptimal.com
+ * Description:       AdsOptimal offers innovative ad unit, designed and optimized for mobile sites. We make the ad load faster and make it easier on various smart devices. We also offers desktop ad unit that can be enabled by just simple clicks.
+ * Version:           1.4.0
+ * Author:            team@adsoptimal.com
+ * Author URI:        http://www.adsoptimal.com/company
+ * Text Domain:       adsoptimal-locale
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/AdsOptimal/adsoptimal-wordpress-plugin
+ * WordPress-Plugin-Boilerplate: v2.6.1
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/*----------------------------------------------------------------------------*
+ * Public-Facing Functionality
+ *----------------------------------------------------------------------------*/
+
+/*
+ * @TODO:
+ *
+ * - replace `class-adsoptimal.php` with the name of the plugin's class file
+ *
+ */
+require_once( plugin_dir_path( __FILE__ ) . 'public/class-adsoptimal.php' );
+
+/*
+ * Register hooks that are fired when the plugin is activated or deactivated.
+ * When the plugin is deleted, the uninstall.php file is loaded.
+ *
+ * @TODO:
+ *
+ * - replace Plugin_Name with the name of the class defined in
+ *   `class-adsoptimal.php`
+ */
+register_activation_hook( __FILE__, array( 'AdsOptimal', 'activate' ) );
+register_deactivation_hook( __FILE__, array( 'AdsOptimal', 'deactivate' ) );
+
+/*
+ * @TODO:
+ *
+ * - replace Plugin_Name with the name of the class defined in
+ *   `class-adsoptimal.php`
+ */
+add_action('plugins_loaded', array('AdsOptimal', 'get_instance'));
+add_action('wp_head', 'adsoptimal_script_head');
+
+add_action('loop_start', 'adsoptimal_loop_start');
+add_action('loop_end', 'adsoptimal_loop_end');
+add_filter('the_content', 'adsoptimal_content');
+
+/*----------------------------------------------------------------------------*
+ * Dashboard and Administrative Functionality
+ *----------------------------------------------------------------------------*/
+
+/*
+ * @TODO:
+ *
+ * - replace `class-adsoptimal-admin.php` with the name of the plugin's admin file
+ * - replace Plugin_Name_Admin with the name of the class defined in
+ *   `class-adsoptimal-admin.php`
+ *
+ * If you want to include Ajax within the dashboard, change the following
+ * conditional to:
+ *
+ * if ( is_admin() ) {
+ *   ...
+ * }
+ *
+ * The code below is intended to to give the lightest footprint possible.
+ */
+if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
+
+	require_once( plugin_dir_path( __FILE__ ) . 'admin/class-adsoptimal-admin.php' );
+	add_action( 'plugins_loaded', array( 'AdsOptimal_Admin', 'get_instance' ) );
+
+}
+
+function adsoptimal_script_head() {
+	if (get_option('adsoptimal_settings', '') != '') {
+		$object = json_decode(rawurldecode(get_option('adsoptimal_settings', '{"jscode": ""}')), true);
+		if ($object && array_key_exists('jscode', $object)) {
+			echo $object['jscode'];
+		}
+	}
+	if (get_option('adsoptimal_enable_desktop_ad', 'false') == 'true') {
+		?>
+<script type='text/javascript'>
+(function(w) {
+w.MobileMonetizer=w.MobileMonetizer||{}; var t={ID:'<?php echo get_option('adsoptimal_publisher_id', '') ?>'};
+for(var a in t) w.MobileMonetizer[a]=t[a];
+var d=document,h=d.getElementsByTagName('head')[0],j=d.createElement('script');
+j.setAttribute('src','//www.adsoptimal.com/advertisement/manual.js');
+h.appendChild(j);
+})(window);
+</script>
+		<?php
+	}
+}
+
+function adsoptimal_loop_start(&$wp_query) {
+	if (get_option('adsoptimal_enable_desktop_ad', 'false') == 'false') return;
+	if (get_option('adsoptimal_top_ad_type', '') == '') return;
+	if (is_single() && get_option('adsoptimal_enable_post_ad', 'true') == 'false') return;
+	if (!is_single() && get_option('adsoptimal_enable_page_ad', 'true') == 'false') return;
+	
+	global $wp_the_query;
+	if (($wp_query === $wp_the_query) && !is_admin() && !is_feed() && !is_robots() && !is_trackback()) {
+		$size = split('x', get_option('adsoptimal_top_ad_type', ''));
+		echo '<div style="text-align:'.get_option('adsoptimal_top_ad_alignment', 'center').'; padding: 15px 0;"><div class="adsoptimal-slot" style="width: '.$size[0].'px; height: '.$size[1].'px; display: inline-block;"></div></div>';
+	}
+}
+function adsoptimal_loop_end(&$wp_query) {
+	if (get_option('adsoptimal_enable_desktop_ad', 'false') == 'false') return;
+	if (get_option('adsoptimal_footer_ad_type', '') == '') return;
+	if (is_single() && get_option('adsoptimal_enable_post_ad', 'true') == 'false') return;
+	if (!is_single() && get_option('adsoptimal_enable_page_ad', 'true') == 'false') return;
+	
+	global $wp_the_query;
+	if (($wp_query === $wp_the_query) && !is_admin() && !is_feed() && !is_robots() && !is_trackback()) {
+		$size = split('x', get_option('adsoptimal_footer_ad_type', ''));
+		echo '<div style="text-align:'.get_option('adsoptimal_footer_ad_alignment', 'center').'; padding: 15px 0;"><div class="adsoptimal-slot" style="width: '.$size[0].'px; height: '.$size[1].'px; display: inline-block;"></div></div>';
+	}
+}
+
+class SharedParams {
+	public $countWord = 0;
+	public $foundDot = false;
+	public $insertCount = 0;
+	public $targetAt = 100;
+	function __construct() {
+		$this->targetAt = intval(get_option('adsoptimal_content_ad_every', '100'));
+	}
+}
+function addContentAd(DOMNode $domNode, $sharedParams) {
+	foreach ($domNode->childNodes as $node)
+	{
+		if ($sharedParams->insertCount >= 2) return;
+		
+		if ($node->nodeType == XML_TEXT_NODE) {
+			$splitedText = split(' ', $node->nodeValue);
+			for ($i=0; $i<count($splitedText); $i++) {
+				$sharedParams->countWord += 1;
+				if ($sharedParams->countWord >= $sharedParams->targetAt) {
+					if ($sharedParams->foundDot) {
+						$splitedText[$i] = '[ADSOPTIMAL_AD_TAG]'.$splitedText[$i];
+						$sharedParams->insertCount += 1;
+						$sharedParams->countWord = 0;
+						$sharedParams->foundDot = false;
+					}
+					elseif (strpos($splitedText[$i], '.') !== false) {
+						$sharedParams->foundDot = true;
+					}
+				}
+			}
+			$node->nodeValue = join(' ', $splitedText);
+		}
+		
+		if($node->hasChildNodes()) {
+			addContentAd($node, $sharedParams);
+		}
+	}    
+}
+
+function adsoptimal_content($content = '') {
+	if (get_option('adsoptimal_enable_desktop_ad', 'false') == 'false') return $content;
+	if (get_option('adsoptimal_content_ad_type', '') == '') return $content;
+	if (is_single() && get_option('adsoptimal_enable_post_ad', 'true') == 'false') return $content;
+	if (!is_single() && get_option('adsoptimal_enable_page_ad', 'true') == 'false') return $content;
+	if (!is_single()) return $content;
+	
+	if (!is_admin() && !is_feed() && !is_robots() && !is_trackback()) {
+		$size = split('x', get_option('adsoptimal_content_ad_type', ''));
+		$adHtml = '<div style="text-align:'.get_option('adsoptimal_content_ad_alignment', 'center').'; padding: 15px 0;"><div class="adsoptimal-slot" style="width: '.$size[0].'px; height: '.$size[1].'px; display: inline-block;"></div></div>';
+		
+		$sharedParams = new SharedParams();
+		$dom = new DOMDocument();
+		$dom->loadHTML('<div>'.$content.'</div>');
+		addContentAd($dom, $sharedParams);
+		
+		$html = $dom->saveHTML();
+		$html = str_replace('[ADSOPTIMAL_AD_TAG]', $adHtml, $html);
+		
+		return $html;
+	}
+	else return $content;
+}
+?>
